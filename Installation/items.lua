076d78b9ee90e642e94bc946ef497c560cbf5b6d
Catpunk18 <155788171+Catpunk18@users.noreply.github.com>
2025-10-19 12:55:25 -0400

    Installation items

---
+++ Installation/items.lua
@@ -0,0 +1,70 @@
+--Copy and paste the below code into items.lua in ox_inventory to use my base script fruits
+["apple"] = {
+	label = "Apple",
+	weight = 100,
+	stack = true,
+	close = true,
+},
+
+["orange"] = {
+	label = "Orange",
+	weight = 100,
+	stack = true,
+	close = true,
+},
+
+["banana"] = {
+	label = "Banana",
+	weight = 120,
+	stack = true,
+	close = true,
+},
+
+["pear"] = {
+	label = "Pear",
+	weight = 100,
+	stack = true,
+	close = true,
+},
+
+["lemon"] = {
+	label = "Lemon",
+	weight = 90,
+	stack = true,
+	close = true,
+},
+
+["lime"] = {
+	label = "Lime",
+	weight = 90,
+	stack = true,
+	close = true,
+},
+
+["mango"] = {
+	label = "Mango",
+	weight = 150,
+	stack = true,
+	close = true,
+},
+
+["peach"] = {
+	label = "Peach",
+	weight = 120,
+	stack = true,
+	close = true,
+},
+
+["pineapple"] = {
+	label = "Pineapple",
+	weight = 250,
+	stack = true,
+	close = true,
+},
+
+["coconut"] = {
+	label = "Coconut",
+	weight = 300,
+	stack = true,
+	close = true,
+},

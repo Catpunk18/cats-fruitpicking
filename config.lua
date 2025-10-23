95e38b476bcbb0630336f6fb0647bed3f83302c1
Catpunk18 <155788171+Catpunk18@users.noreply.github.com>
2025-10-23 18:47:50 -0400

    Config

---
+++ config.lua
@@ -0,0 +1,176 @@
+Config = {}
+
+Config.TreeCooldown = 180 -- seconds
+
+Config.Trees = {
+    {
+        fruit = 'apple',
+        label = 'Apple Tree',
+        anim = { dict = 'amb@prop_human_movie_bulb@idle_a', name = 'idle_a' },
+        coords = {
+            vec3(2423.673, 4658.729, 34.436),
+            vec3(2419.635, 4673.898, 34.839),
+            vec3(2407.242, 4676.875, 34.899),
+            vec3(2401.747, 4688.114, 34.578),
+            vec3(2389.989, 4690.985, 34.726),
+            vec3(2381.694, 4700.502, 34.701),
+            vec3(2383.314, 4712.957, 34.661),
+            vec3(2366.798, 4715.727, 35.228),
+            vec3(2364.978, 4729.535, 34.834),
+            vec3(2359.362, 4723.812, 35.461),
+            vec3(2350.595, 4734.004, 35.466),
+            vec3(2339.590, 4741.310, 35.731),
+            vec3(2324.765, 4746.820, 36.846)
+        }
+    },
+    {
+        fruit = 'orange',
+        label = 'Orange Tree',
+        anim = { dict = 'amb@prop_human_movie_bulb@idle_a', name = 'idle_a' },
+        coords = {
+            vec3(2146.207, 4867.706, 41.666),
+            vec3(2123.139, 4883.858, 41.545),
+            vec3(2122.070, 4861.961, 41.876),
+            vec3(2102.187, 4877.729, 41.936),
+            vec3(2117.445, 4842.232, 42.293),
+            vec3(2098.273, 4841.499, 42.460),
+            vec3(2083.318, 4853.496, 42.612),
+            vec3(2086.261, 4825.850, 42.446),
+            vec3(2064.070, 4819.331, 42.779),
+            vec3(2060.305, 4842.852, 42.664),
+            vec3(2031.254, 4801.941, 42.584),
+            vec3(2016.101, 4800.429, 42.856),
+            vec3(2003.703, 4787.041, 42.579),
+            vec3(1981.992, 4771.769, 42.915)
+        }
+    },
+    {
+        fruit = 'pear',
+        label = 'Pear Tree',
+        anim = { dict = 'amb@prop_human_movie_bulb@idle_a', name = 'idle_a' },
+        coords = {
+            vec3(2327.706, 4770.738, 36.655),
+            vec3(2325.919, 4761.901, 36.849),
+            vec3(2339.305, 4767.262, 35.869),
+            vec3(2343.455, 4755.708, 35.395),
+            vec3(2353.485, 4760.438, 35.008),
+            vec3(2367.013, 4751.444, 34.448),
+            vec3(2374.540, 4735.208, 34.407),
+            vec3(2386.770, 4736.281, 33.955),
+            vec3(2386.867, 4724.585, 34.232),
+            vec3(2402.254, 4717.324, 33.897),
+            vec3(2404.345, 4704.050, 34.317),
+            vec3(2412.637, 4707.369, 33.667),
+            vec3(2423.895, 4697.931, 33.563),
+            vec3(2422.175, 4686.816, 34.454),
+            vec3(2434.223, 4678.816, 34.289),
+            vec3(2443.394, 4672.414, 33.915)
+        }
+    },
+    {
+        fruit = 'lemon',
+        label = 'Lemon Tree',
+        anim = { dict = 'amb@prop_human_movie_bulb@idle_a', name = 'idle_a' },
+        coords = {
+            vec3(2317.794, 4984.469, 42.896),
+            vec3(2331.464, 4996.539, 42.855),
+            vec3(2344.393, 5007.830, 43.536),
+            vec3(2357.122, 5020.401, 44.565),
+            vec3(2343.831, 5022.586, 44.395),
+            vec3(2331.100, 5007.878, 43.157),
+            vec3(2317.179, 4994.425, 42.725),
+            vec3(2304.966, 4996.968, 43.028),
+            vec3(2316.796, 5008.796, 43.407),
+            vec3(2330.299, 5021.694, 43.857),
+            vec3(2341.714, 5034.873, 45.180),
+            vec3(2329.502, 5036.991, 45.481),
+            vec3(2316.628, 5023.604, 44.466)
+        }
+    },
+    {
+        fruit = 'lime',
+        label = 'Lime Tree',
+        anim = { dict = 'amb@prop_human_movie_bulb@idle_a', name = 'idle_a' },
+        coords = {
+            vec3(2376.125, 5016.871, 46.174),
+            vec3(2389.686, 5004.717, 46.443),
+            vec3(2390.021, 4992.610, 46.091),
+            vec3(2377.683, 5003.899, 45.504),
+            vec3(2369.618, 5010.993, 45.176),
+            vec3(2360.926, 5002.414, 44.454),
+            vec3(2373.929, 4989.054, 45.083),
+            vec3(2361.660, 4988.816, 44.161),
+            vec3(2361.525, 4976.489, 44.290),
+            vec3(2349.593, 4989.422, 43.904),
+            vec3(2349.337, 4975.909, 43.530),
+            vec3(2336.368, 4976.147, 43.494)
+        }
+    },
+    {
+        fruit = 'peach',
+        label = 'Peach Tree',
+        anim = { dict = 'amb@prop_human_movie_bulb@idle_a', name = 'idle_a' },
+        coords = {
+            vec3(1986.51, 4961.47, 43.50),
+            vec3(1988.38, 4963.65, 43.52),
+            vec3(1990.25, 4965.83, 43.54)
+        }
+    },
+    {
+        fruit = 'mango',
+        label = 'Mango Tree',
+        anim = { dict = 'amb@prop_human_movie_bulb@idle_a', name = 'idle_a' },
+        coords = {
+            vec3(1980.92, 4954.91, 43.44),
+            vec3(1982.80, 4957.10, 43.46),
+            vec3(1984.65, 4959.28, 43.48)
+        }
+    },
+    {
+        fruit = 'banana',
+        label = 'Banana Tree',
+        anim = { dict = 'amb@prop_human_movie_bulb@idle_a', name = 'idle_a' },
+        coords = {
+            vec3(1958.31, 4928.78, 43.18),
+            vec3(1960.10, 4931.00, 43.20),
+            vec3(1962.25, 4933.42, 43.22)
+        }
+    },
+    {
+        fruit = 'pineapple',
+        label = 'Pineapple Plant',
+        anim = { dict = 'amb@world_human_gardener_plant@male@base', name = 'base' },
+        coords = {
+            vec3(1992.12, 4968.01, 43.56),
+            vec3(1993.98, 4970.20, 43.58),
+            vec3(1995.85, 4972.38, 43.60)
+        }
+    },
+    {
+        fruit = 'coconut',
+        label = 'Coconut Palm',
+        anim = { dict = 'amb@prop_human_movie_bulb@idle_a', name = 'idle_a' },
+        coords = {
+            vec3(1997.72, 4974.56, 43.62),
+            vec3(1999.58, 4976.75, 43.64),
+            vec3(2001.45, 4978.93, 43.66)
+        }
+    },
+}
+
+Config.FruitRewards = {
+    ['apple'] = { item = 'apple', amount = 1 },
+    ['orange'] = { item = 'orange', amount = 1 },
+    ['lemon'] = { item = 'lemon', amount = 1 },
+    ['lime'] = { item = 'lime', amount = 1 },
+    ['pear'] = { item = 'pear', amount = 1 },
+    ['peach'] = { item = 'peach', amount = 1 },
+    ['banana'] = { item = 'banana', amount = 1 },
+    ['mango'] = { item = 'mango', amount = 1 },
+    ['pineapple'] = { item = 'pineapple', amount = 1 },
+    ['coconut'] = { item = 'coconut', amount = 1 }
+}
+
+
+--add grapefruit, avacado, cherry, olive for cooking oil?, pomegranate
+--52
